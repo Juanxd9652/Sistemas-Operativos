90843961a4abc1f8be28c0d22ff12ec330bb45c5
Juanxd9652 <juan.lopez.cuellar@unillanos.edu.co>
2022-07-07 12:48:03 -0500

    Avance 1

--- Pruebaaaa.cpp
+++ Pruebaaaa.cpp
@@ -124,7 +124,7 @@ void FCFS(int Paquete[][3], int NProcesos){
     system("cls");
             for (int b=0;b<NProcesos;b++){
                 for (int a=0;a<1;a++){
-                    TCPU=Paquete[b][0];
+                    TCPU=Paquete[b][1];
                     if (b==0){
                         TiempoDeEspera[b]=0;
                     }else{
@@ -155,15 +155,95 @@ void FCFS(int Paquete[][3], int NProcesos){
 
 };
 
-void SJF(int SJF1[][3], int NProcesos){
+void SJF(int Paquete[][3], int NProcesos){
+
+    int TiempoCPU[100];
+    int TiempoDeEspera[100];
+    TiempoDeEspera[0]=0;
+    char TCPU1[6];
+    int TCPU=0;
+    float TiempoPromedioDeEspera=0;
+
+    int SJF[2][20]; //Matriz para guardar el orden de llegada y el TCPU.
+    int aux[2][2]; //Matriz auxiliar para ordenar los TCPU de menor a mayor.
+    system("cls");
+
+    // Aquí se insertan los tiempos de CPU
+
+    for (int b=0;b<NProcesos;b++){
+        for (int a=0;a<1;a++){
+            SJF[0][b]=Paquete[b][1]; //T CPU
+            SJF[1][b]=Paquete[b][0]; //Identificador
+
+        }
+    }
+
+    //Método de ordenamiento
+
+    int cambio=0;
+    for (int a=0;a<1;){
+        for (int b=0;b<NProcesos-1;b++){
+            if (SJF[0][b]>SJF[0][b+1]){
+                aux[0][0]=SJF[1][b];
+                aux[0][1]=SJF[0][b];
+                aux[1][0]=SJF[1][b+1];
+                aux[1][1]=SJF[0][b+1];
+
+                SJF[1][b]=aux[1][0];
+                SJF[1][b+1]=aux[0][0];
+                SJF[0][b]=aux[1][1];
+                SJF[0][b+1]=aux[0][1];
+
+                cambio=1;
+            }
+        }
+        if (cambio==0){
+            a=1;
+        }
+        cambio=0;
+    }
+
+    // Se registra el tiempo de espera de cada proceso
+
+    for (int a=0;a<NProcesos;a++){
+        if (a==0){
+            TiempoDeEspera[a]=0;
+        }else{
+            TiempoDeEspera[a]=TiempoDeEspera[a-1]+SJF[0][a-1];
+        }
+    }
+    /*
+    for (int a=0;a<NProcesos;a++){
+        cout<<TiempoDeEspera[a]<<endl;
+    }
+    */
+
+    // Se calcula el tiempo de espera promedio
+
+    for (int a=0;a<NProcesos;a++){
+        TiempoPromedioDeEspera+=TiempoDeEspera[a];
+        if(a==NProcesos-1){
+            TiempoPromedioDeEspera/=NProcesos;
+        }
+    }
+
+    cout<<"\nEl tiempo promedio de espera es de: "<<TiempoPromedioDeEspera<<" ms.\n"<<endl;
+    cout<<"---------DIAGRAMA DE GANTT---------\n\n";
+    cout<<"       0 ms."<<endl;
+    for(int a=0,b=0;a<NProcesos;a++){
+        cout<<"       ^\n       |\n       P"<<SJF[1][a]<<"\n       |\n       v"<<endl;
+        b+=SJF[0][a];
+        cout<<"       "<<b<<" ms."<<endl;
+    }
+    system("pause");
+    system("cls");
+};
+
+void Prioridad(int Paquete[][3], int NProcesos){
     cout<<"Funciona FCFS";
 };
 
-void Prioridad(int Prioridad1[][3], int NProcesos){
-    cout<<"Funciona FCFS";
-};
-
-void RoundRobin(int RoundRobin1[][3], int NProcesos, int Quantum){
+void RoundRobin(int Paquete[][3], int NProcesos, int Quantum){
     cout<<"Funciona RoundRobin";
 };
 
@@ -198,6 +278,7 @@ void Generacion1(){
 
         int Q_A;
         int Q_B;
+        int auxQ;
         int Quantum;
 
     do{
@@ -253,15 +334,15 @@ void Generacion1(){
         cout<<"\nInserte los rangos de generación para el Quantum.\nPRECAUCIÓN: No sobrecargue el programa, podrá asignar un Quantum máximo de .\n\n";
         cout<<"Rango inferior: ";
         cin>>RangoA;
-        P_A=atoi(RangoA);
+        Q_A=atoi(RangoA);
         cout<<"\nRango superior: ";
         cin>>RangoB;
-        P_B=atoi(RangoB);
-    }while(P_A>P_B || P_A<=0 || P_B>20 || validarNumeros(RangoA) || validarNumeros(RangoB));
+        Q_B=atoi(RangoB);
+    }while(Q_A>Q_B || Q_A<=0 || Q_B>20 || validarNumeros(RangoA) || validarNumeros(RangoB));
 
-    auxP = P_B - P_A + 1;
-    NProcesos=P_A + rand()%auxP;
-    cout<<"\nEl número aleatorio de procesos generado es: "<<NProcesos<<"\n\n";
+    auxQ = Q_B - Q_A + 1;
+    Quantum=Q_A + rand()%auxQ;
+    cout<<"\nEl número aleatorio de procesos generado es: "<<Quantum<<"\n\n";
     system("pause");
 
     // Vamos a llenar la matriz a trabajar
@@ -289,6 +370,7 @@ void Generacion1(){
             cout<<PaqueteDatos[a-1][2]<<endl;
         }
     system("pause");
+
     FCFS(PaqueteDatos,NProcesos);
     SJF(PaqueteDatos,NProcesos);
     Prioridad(PaqueteDatos,NProcesos);
