1ecd8a23e43a67f272ae0f62dc9d465458dd21ad
Juanxd9652 <juan.lopez.cuellar@unillanos.edu.co>
2022-07-11 22:01:47 -0500

    Ya casito

--- Pruebaaaa.cpp
+++ Pruebaaaa.cpp
@@ -15,6 +15,7 @@ int validarNombres(char num[]); //Para validar que los nombres no contengan núme
 int validarNumeros(char num[]); //Para validar que los números no contengan letras.
 void Generacion1(); //Genera los números aleatorios con un rango definido.
 void Generacion2(); //Genera los números aleatorios con un rango definido por el sistema.
+void Generacion3(); //Permite insertar al usuario los datos manualmente.
 
 void FCFS(int Paquete[][3], int NProcesos);
 void SJF(int Paquete[][3], int NProcesos);
@@ -70,8 +71,11 @@ int main(){
                 Generacion2();
             }
 
+        }else{
+            cout<<"Aquí debo poner texto haciendo referencia a la inserción de datos de manera manual.\n";
+            Generacion3();
         }
-        cout<<"Validó que no ingresaran letras y que el rango de opciones sea el correcto.\n";
+        cout<<"\nValidó que no ingresaran letras y que el rango de opciones sea el correcto.\n";
 
         system("pause");
         system("cls");
@@ -113,70 +117,63 @@ int validarNumeros(char num[]){
 }
 
 void FCFS(int Paquete[][3], int NProcesos){
+
     int TiempoCPU[100];
     int TiempoDeEspera[100];
     TiempoDeEspera[0]=0;
-    char NProcesos1[3];
-    char TCPU1[6];
     int TCPU=0;
     int aux;
     float TiempoPromedioDeEspera=0;
     system("cls");
-            for (int b=0;b<NProcesos;b++){
-                for (int a=0;a<1;a++){
-                    TCPU=Paquete[b][1];
-                    if (b==0){
-                        TiempoDeEspera[b]=0;
-                    }else{
-                        TiempoDeEspera[b]=TiempoDeEspera[b-1]+TiempoCPU[b-1];
-                    }
 
-                    TiempoCPU[b]=TCPU;
-                }
-            }
+    // Para guardar el tiempo de espera de cada proceso.
 
-            for (int a=0;a<NProcesos;a++){
-                TiempoPromedioDeEspera+=TiempoDeEspera[a];
-                if(a==NProcesos-1){
-                    TiempoPromedioDeEspera/=NProcesos;
-                }
-            }
-            Titulo();
-            cout<<"  -----------------------------\n";
-            cout<<"  |DIAGRAMA DE GANTT PARA FCFS|\n";
-            cout<<"  -----------------------------\n\n";
-            cout<<"              0 ms."<<endl;
-            for(int a=0,b=0;a<NProcesos;a++){
-                cout<<"              ^\n              |\n              P"<<a+1<<"\n              |\n              v"<<endl;
-                b+=TiempoCPU[a];
-                cout<<"              "<<b<<" ms."<<endl;
-            }
-            cout<<"\n  * El tiempo promedio de espera es de: "<<TiempoPromedioDeEspera<<" ms.\n"<<endl;
-            //system("pause");
+    for (int b=0;b<NProcesos;b++){
+        TCPU=Paquete[b][1];
+        if (b==0){
+            TiempoDeEspera[b]=0;
+        }else{
+            TiempoDeEspera[b]=TiempoDeEspera[b-1]+TiempoCPU[b-1];
+        }
+        TiempoCPU[b]=TCPU;
+    }
 
+    // Para calcular el tiempo promedio de espera.
+
+    for (int a=0;a<NProcesos;a++){
+        TiempoPromedioDeEspera+=TiempoDeEspera[a];
+        if(a==NProcesos-1){
+            TiempoPromedioDeEspera/=NProcesos;
+        }
+    }
+
+    Titulo();
+    cout<<"  -----------------------------\n";
+    cout<<"  |DIAGRAMA DE GANTT PARA FCFS|\n";
+    cout<<"  -----------------------------\n\n";
+    cout<<"              0 ms."<<endl;
+    for(int a=0,b=0;a<NProcesos;a++){
+        cout<<"              ^\n              |\n              P"<<a+1<<"\n              |\n              v"<<endl;
+        b+=TiempoCPU[a];
+        cout<<"              "<<b<<" ms."<<endl;
+    }
+    cout<<"\n  * El tiempo promedio de espera es de: "<<TiempoPromedioDeEspera<<" ms.\n"<<endl;
 };
 
 void SJF(int Paquete[][3], int NProcesos){
 
-    int TiempoCPU[100];
     int TiempoDeEspera[100];
     TiempoDeEspera[0]=0;
-    char TCPU1[6];
-    int TCPU=0;
     float TiempoPromedioDeEspera=0;
 
     int SJF[2][20]; //Matriz para guardar el orden de llegada y el TCPU.
     int aux[2][2]; //Matriz auxiliar para ordenar los TCPU de menor a mayor.
-    //system("cls");
 
     // Aquí se insertan los tiempos de CPU
 
     for (int b=0;b<NProcesos;b++){
-        for (int a=0;a<1;a++){
-            SJF[0][b]=Paquete[b][1]; //T CPU
-            SJF[1][b]=Paquete[b][0]; //Identificador
-
-        }
+        SJF[0][b]=Paquete[b][1]; //T CPU
+        SJF[1][b]=Paquete[b][0]; //Identificador
     }
 
     //Método de ordenamiento
@@ -213,11 +210,6 @@ void SJF(int Paquete[][3], int NProcesos){
             TiempoDeEspera[a]=TiempoDeEspera[a-1]+SJF[0][a-1];
         }
     }
-    /*
-    for (int a=0;a<NProcesos;a++){
-        cout<<TiempoDeEspera[a]<<endl;
-    }
-    */
 
     // Se calcula el tiempo de espera promedio
 
@@ -238,12 +230,81 @@ void SJF(int Paquete[][3], int NProcesos){
         cout<<"              "<<b<<" ms."<<endl;
     }
     cout<<"\n  * El tiempo promedio de espera es de: "<<TiempoPromedioDeEspera<<" ms.\n"<<endl;
-    system("pause");
-    system("cls");
 };
 
 void Prioridad(int Paquete[][3], int NProcesos){
-    cout<<"Funciona FCFS";
+
+    int TiempoDeEspera[100];
+    TiempoDeEspera[0]=0;
+    int TCPU=0;
+    float TiempoPromedioDeEspera=0;
+
+    int Prioridad[3][100]; //Matriz para guardar el orden de llegada, la prioridad y el TCPU.
+    int aux2[3][2]; //Matriz auxiliar para ordenar los procesos según la prioridad.
+
+    // Aquí se insertan los datos del Paquete.
+
+    for (int b=0;b<NProcesos;b++){
+        Prioridad[1][b]=Paquete[b][0]; // Identificador
+        Prioridad[2][b]=Paquete[b][1]; // TCPU.
+        Prioridad[0][b]=Paquete[b][2]; // Prioridad
+    }
+
+    //Método de ordenamiento
+
+    int cambio=0;
+    for (int a=0;a<1;){
+        for (int b=0;b<NProcesos-1;b++){
+            if (Prioridad[0][b]>Prioridad[0][b+1]){
+                aux2[0][0]=Prioridad[0][b]; //Guarda prioridad
+                aux2[0][1]=Prioridad[0][b+1];
+                aux2[1][0]=Prioridad[1][b]; //Guarda orden
+                aux2[1][1]=Prioridad[1][b+1];
+                aux2[2][0]=Prioridad[2][b]; //Guarda TCPU
+                aux2[2][1]=Prioridad[2][b+1];
+
+                Prioridad[0][b]=aux2[0][1];
+                Prioridad[0][b+1]=aux2[0][0];
+                Prioridad[1][b]=aux2[1][1];
+                Prioridad[1][b+1]=aux2[1][0];
+                Prioridad[2][b]=aux2[2][1];
+                Prioridad[2][b+1]=aux2[2][0];
+
+                cambio=1;
+            }
+        }
+        if (cambio==0){
+            a=1;
+        }
+        cambio=0;
+    }
+
+    for (int a=0;a<NProcesos;a++){
+        if (a==0){
+            TiempoDeEspera[a]=0;
+        }else{
+            TiempoDeEspera[a]=TiempoDeEspera[a-1]+Prioridad[2][a-1];
+        }
+    }
+
+    for (int a=0;a<NProcesos;a++){
+        TiempoPromedioDeEspera+=TiempoDeEspera[a];
+        if(a==NProcesos-1){
+            TiempoPromedioDeEspera/=NProcesos;
+        }
+    }
+
+    cout<<"  ----------------------------------\n";
+    cout<<"  |DIAGRAMA DE GANTT PARA PRIORIDAD|\n";
+    cout<<"  ----------------------------------\n\n";
+    cout<<"              0 ms."<<endl;
+    for(int a=0,b=0;a<NProcesos;a++){
+        cout<<"              ^\n              |\n              P"<<Prioridad[1][a]<<"\n              |\n              v"<<endl;
+        b+=Prioridad[2][a];
+        cout<<"              "<<b<<" ms."<<endl;
+    }
+    cout<<"\nEl tiempo promedio de espera es de: "<<TiempoPromedioDeEspera<<" ms.\n"<<endl;
+
 };
 
 void RoundRobin(int Paquete[][3], int NProcesos, int Quantum){
@@ -298,7 +359,7 @@ void Generacion1(){
 
     auxP = P_B - P_A + 1;
     NProcesos=P_A + rand()%auxP;
-    cout<<"\nEl número de procesos generado de manera aleatoria es ("<<NProcesos<<").\n\n";
+    cout<<"\nEl número de procesos que fue generado de manera aleatoria es ("<<NProcesos<<").\n\n";
     system("pause");
 
     do{
@@ -345,7 +406,7 @@ void Generacion1(){
 
     auxQ = Q_B - Q_A + 1;
     Quantum=Q_A + rand()%auxQ;
-    cout<<"\nEl número aleatorio de procesos generado es: "<<Quantum<<"\n\n";
+    cout<<"\nEl Quantum que fue generado de manera aleatoria es ("<<Quantum<<").\n\n";
     system("pause");
 
     // Vamos a llenar la matriz a trabajar
@@ -380,10 +441,174 @@ void Generacion1(){
     RoundRobin(PaqueteDatos,NProcesos,Quantum);
 };
 
+void Generacion3(){
+
+    //Variables necesarias
+
+    char Ingreso[2];
+    int PaqueteDatos[20][3];
+
+        //Variables para # Procesos
+
+        int NProcesos;
+
+        //Variables para T CPU
+
+        int T_A;
+
+        //Variables para Prioridad
+
+        int Pr_A;
+        int auxPr;
+
+        //Variables para Quantum
+
+        int Quantum;
+
+    do{
+        system("cls");
+        Titulo();
+        cout<<"\nInserte la cantidad de procesos a trabajar.\nPRECAUCIÓN: No sobrecargue el programa, podrá generar hasta un máximo de 20 procesos.\n\n";
+        cout<<"Procesos: ";
+        cin>>Ingreso;
+        NProcesos=atoi(Ingreso);
+    }while(NProcesos>20 || NProcesos<=0 || validarNumeros(Ingreso));
+
+    cout<<"\nEl número de procesos ingresado es ("<<NProcesos<<").\n\n";
+    system("pause");
+
+    // Primero, el identificador
+
+        for (int a=1;a<=NProcesos;a++){
+            PaqueteDatos[a-1][0]=a;
+            cout<<PaqueteDatos[a-1][0]<<endl;
+        }
+
+    for (int a=0;a<NProcesos;a++){
+        do{
+        system("cls");
+        Titulo();
+        cout<<"\nInserte los tiempos de CPU de cada proceso.\nPRECAUCIÓN: No sobrecargue el programa, podrá asignar hasta un máximo de 20 milisegundos por proceso.\n\n";
+        cout<<"Tiempo de CPU del proceso "<<a+1<<": ";
+        cin>>Ingreso;
+        T_A=atoi(Ingreso);
+        }while(T_A>20 || T_A<=0 || validarNumeros(Ingreso));
+        PaqueteDatos[a-1][1]=T_A;
+    }
+
+    system("pause");
+
+    for (int a=0;a<NProcesos;a++){
+        do{
+        system("cls");
+        Titulo();
+        cout<<"\nInserte las prioridades de cada proceso.\nPRECAUCIÓN: No sobrecargue el programa, podrá generar hasta un máximo de 10 prioridades.\n\n";
+        cout<<"Prioridad del proceso ("<<a+1<<"): ";
+        cin>>Ingreso;
+        Pr_A=atoi(Ingreso);
+        }while(Pr_A>10 || Pr_A<=0 || validarNumeros(Ingreso));
+        PaqueteDatos[a-1][2]=Pr_A;
+    }
+
+    system("pause");
+
+    do{
+        system("cls");
+        Titulo();
+        cout<<"\nInserte el Quantum.\nPRECAUCIÓN: No sobrecargue el programa, podrá asignar un Quantum máximo de 10 ms .\n\n";
+        cout<<"Quantum: ";
+        cin>>Ingreso;
+        Quantum=atoi(Ingreso);
+    }while(Quantum>10 || Quantum<=0 || validarNumeros(Ingreso));
+
+    cout<<"\nEl Quantum ingresado es ("<<Quantum<<").\n\n";
+
+    system("pause");
+
+    FCFS(PaqueteDatos,NProcesos);
+    SJF(PaqueteDatos,NProcesos);
+    Prioridad(PaqueteDatos,NProcesos);
+    RoundRobin(PaqueteDatos,NProcesos,Quantum);
+};
+
 void Generacion2(){
-    cout<<"Nuevo cambio al código";
-    cout<<"Un cambio más";
-    cout<<"Hola";
+    /*
+    int TiempoCPU[100];
+    int TiempoDeEspera[100];
+    TiempoDeEspera[0]=0;
+    char NProcesos1[3];
+    int NProcesos;
+    char TCPU1[6];
+    int TCPU=0;
+    char Prioridad1[6];
+    int Prio=0;
+
+    int Prioridad[3][100]; //Matriz para guardar el orden de llegada, la prioridad y el TCPU.
+    int aux2[3][2]; //Matriz auxiliar para ordenar los procesos según la prioridad.
+    system("cls");
+
+    for (int a=0;a<1;){
+    cout<<"\nIndique la cantidad de procesos a ingresar (Máximo 100 procesos) para Prioridad: ";
+        cin>>NProcesos1;
+        if(validar(NProcesos1)){
+            system("cls");
+            cout<<"Error: Solo se permiten números.\n"<<endl;
+        }
+        else{
+            NProcesos=atoi(NProcesos1); //Para convertir el char en int.
+            if (NProcesos<1 || NProcesos>100){
+                system("cls");
+                cout<<"Error: Número fuera del rango.\n"<<endl;
+            }else{
+            a++;
+            }
+        }
+    }
+
+    // Aquí se insertan las prioridades de cada proceso.
+
+    cout<<endl;
+    for (int b=0;b<NProcesos;b++){
+        for (int a=0;a<1;){
+            cout<<"\nIndique la prioridad del proceso P"<<b+1<<": ";
+            cin>>Prioridad1;
+            if(validar(Prioridad1)){
+                cout<<"\nError: Solo se permiten números.\n"<<endl;
+            }
+            else{
+                Prio=atoi(Prioridad1); //Para convertir el char en int.
+                if (Prio<=0){
+                    cout<<"\nError: Prioridad no válida.\n"<<endl;
+                }else{
+                a++;
+                }
+            }
+            Prioridad[0][b]=Prio;
+            Prioridad[1][b]=b+1;
+        }
+    }
+
+    // Aquí se insertan los tiempos de CPU
+
+    for (int b=0;b<NProcesos;b++){
+        for (int a=0;a<1;){
+            cout<<"\nIndique el tiempo de CPU del proceso P"<<b+1<<": ";
+            cin>>TCPU1;
+            if(validar(TCPU1)){
+                cout<<"\nError: Solo se permiten números.\n"<<endl;
+            }
+            else{
+                TCPU=atoi(TCPU1); //Para convertir el char en int.
+                if (TCPU<=0){
+                    cout<<"\nError: Tiempo de CPU no válido.\n"<<endl;
+                }else{
+                    a++;
+                }
+            }
+            Prioridad[2][b]=TCPU;
+        }
+    }
+    */
 };
 
 
